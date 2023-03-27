02d21cdba0b80b6463f7d833946ad6da4af056e2
Julia Salvador <juliasalvador10@gmail.com>
2023-03-27 16:28:25 -0300

    Alterado caminho do autoload.php

--- autoload.php
+++ autoload.php
@@ -2,7 +2,7 @@
 
     spl_autoload_register(function($class) {
         $arquivo = $_SERVER["DOCUMENT_ROOT"]
-        ."/TRABALHO_PHP_POO/classes/". $class. ".class.php";
+        ."/TRABALHO_NOTICIAS/classes/". $class. ".class.php";
     
         if (file_exists($arquivo)) {
             require $arquivo;
